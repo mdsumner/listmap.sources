e6ab6c5f07f7aab860c7dc0398ec1bf532d856b6
Michael Sumner <mdsumner@gmail.com>
2023-07-17 15:59:59 +1000

    write to parquet

--- data-raw/find_list_sources.R
+++ data-raw/find_list_sources.R
@@ -9,7 +9,7 @@ vsizips <- file.path("/vsizip//vsicurl", zips)
 writeLines(vsizips, "data-raw/list.sources.txt")
 
 
-if (FALSE) {
+#if (FALSE) {
   f <- purrr::safely(vapour::vapour_vsi_list)
   l <- purrr::map(vsizips, f)
   bad <- unlist(lapply(l, \(.x) is.null(.x$result)))
@@ -18,19 +18,16 @@ if (FALSE) {
 
   d <- tibble::tibble(url = zips, dsn = vsizips, vsilist = lapply(l, \(.x) .x$result))
   dd <- tidyr::unnest(d, "vsilist")
-  d2 <- dd[tools::file_ext(dd$vsilist) %in% c("asc", "tif", "zip", "gdbtable"), ]
-  dim(d2)
+  writeLines(sort(unique(tools::file_ext(dd$vsilist))), "data-raw/listmap-sources-unique_file_ext.txt")
+  d2 <- dd[tools::file_ext(dd$vsilist) %in% c("asc", "tif", "zip", "gdbtable", "csv", "zip"), ]
 
+  ## make sure we don't include every sub gdb/ file (we might want to detect which are raster/vector)
   gdb <- tools::file_ext(dirname(d2$vsilist)) == "gdb"
   d2$vsilist[gdb] <- dirname(d2$vsilist[gdb])
   library(dplyr)
   d2 <- d2 |> distinct()
-  d2$type <- tools::file_ext(d2$vsilist)
-  dsn <- d2 |> filter(type == "asc") |> mutate(dsn  = file.path(dsn, vsilist)) |> pull(dsn)
 
-
-
-
-}
+arrow::write_parquet(d2, "data-raw/listmap-sources-vsi.parquet")
+#}
 
 
